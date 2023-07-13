2969e187bde77c6720edd55d139e0e907866d1d2
Mario Todorov <mario.yankov.todorov@gmail.com>
2023-07-13 18:01:20 +0300

    Edit solution for homework 4

--- 04.Homework_4/04.Solution_4.c
+++ 04.Homework_4/04.Solution_4.c
@@ -19,6 +19,72 @@
 
 int main(){
     
+    int testMacro_1 = 0;
+    int testMacro_2 = 0;
+    int testBitwiseMacro = 0;
+    putchar('\n');
 
+    // Test MAX
+    testMacro_1 = MAX(3, 5, 7);
+    printf("Test MAX: %d\n", testMacro_1);
+    testMacro_1 = MAX(1, 0, 177);
+    printf("Test MAX: %d\n", testMacro_1);
+    putchar('\n');
+
+    // Test MIN
+    testMacro_2 = MIN(3, 5, 7);
+    printf("Test MIN: %d\n", testMacro_2);
+    testMacro_2 = MIN(1, 0, 100);
+    printf("Test MIN: %d\n", testMacro_2);
+    putchar('\n');
+
+    // Test SETBIT
+    testBitwiseMacro = 0xeeeeeeee;
+    SETBIT(testBitwiseMacro, 0);
+    printf("Test SETBIT: %x\n", testBitwiseMacro);
+    testBitwiseMacro = 0xeeeeeeee;
+    SETBIT(testBitwiseMacro, 4);
+    printf("Test SETBIT: %x\n", testBitwiseMacro);
+    putchar('\n');
+
+    // Test CLEARBIT
+    testBitwiseMacro = 0xffffffff;
+    CLEARBIT(testBitwiseMacro, 0);
+    printf("Test CLEARBIT: %x\n", testBitwiseMacro);
+    testBitwiseMacro = 0xffffffff;
+    CLEARBIT(testBitwiseMacro, 4);
+    printf("Test CLEARBIT: %x\n", testBitwiseMacro);
+    putchar('\n');
+
+    // Test INVERSEBIT
+    testBitwiseMacro = 0xf0f0f0f0;
+    INVERSEBIT(testBitwiseMacro, 8);
+    printf("Test INVERSEBIT: %x\n", testBitwiseMacro);
+    testBitwiseMacro = 0xf0f0f0f0;
+    INVERSEBIT(testBitwiseMacro, 12);
+    printf("Test INVERSEBIT: %x\n", testBitwiseMacro);
+    putchar('\n');
+
+    // Test CHECKBIT
+    testBitwiseMacro = 0xf0f0f0f0;
+    testBitwiseMacro = CHECKBIT(testBitwiseMacro, 8);
+    printf("Test CHECKBIT: %x\n", testBitwiseMacro);
+    testBitwiseMacro = 0xf0f0f0f0;
+    testBitwiseMacro = CHECKBIT(testBitwiseMacro, 12);
+    printf("Test CHECKBIT: %x\n", testBitwiseMacro);
+    putchar('\n');
+
+    // Test SWAP
+    testMacro_1 = 0;
+    testMacro_2 = 1;
+    SWAP(testMacro_1, testMacro_2);
+    printf("Test SWAP: %d %d\n", testMacro_1, testMacro_2);
+    testMacro_1 = 576;
+    testMacro_2 = 100;
+    SWAP(testMacro_1, testMacro_2);
+    printf("Test SWAP: %d %d\n", testMacro_1, testMacro_2);
+
+    putchar('\n');
+    
     return 0;
 }
